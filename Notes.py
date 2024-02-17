1205647037f778165279badbf347161c5f224f7c
Конпаку <sputnik-999-mel@mail.ru>
2024-02-17 13:54:26 +0500

    add

--- Notes.py
+++ Notes.py
@@ -1,3 +1,7 @@
+import json
+import os
+import datetime
+
 def save_notes(notes):
     with open("notes.json", "w") as file:
         json.dump(notes, file)
@@ -8,6 +12,18 @@ def load_notes():
     with open("notes.json", "r") as file:
         return json.load(file)
     
+def add_note():
+    title = input("Введите заголовок заметки: ")
+    body = input("Введите текст заметки: ")
+    note = {
+        "id": len(notes) + 1,
+        "title": title,
+        "body": body,
+        "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    notes.append(note)
+    save_notes(notes)
+    print("Заметка добавлена.")
     
 notes = load_notes()
 
