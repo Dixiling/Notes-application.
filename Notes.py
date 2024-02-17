5b8420731c2027127158a96be9946bce23d23703
Конпаку <sputnik-999-mel@mail.ru>
2024-02-17 13:52:22 +0500

    save/load

--- Notes.py
+++ Notes.py
@@ -1,4 +1,14 @@
+def save_notes(notes):
+    with open("notes.json", "w") as file:
+        json.dump(notes, file)
 
+def load_notes():
+    if not os.path.exists("notes.json"):
+        return []
+    with open("notes.json", "r") as file:
+        return json.load(file)
+    
+    
 notes = load_notes()
 
 while True:
